dc40902549f798a87738ebd82fdea0f9d454c5fa
sujith133 <sujithcharles007@gmail.com>
2026-02-28 14:07:56 +0530

    Restore full VK Max Cleanz website with dual-brand routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import UnderConstruction from './UnderConstruction'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import CleaningHome from './pages/cleaning/CleaningHome';
+import FurnitureHome from './pages/furniture/FurnitureHome';
+import FloatingWhatsApp from './components/shared/FloatingWhatsApp';
+
+function AppContent() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/cleanz" replace />} />
+        <Route path="/cleanz" element={<CleaningHome />} />
+        <Route path="/furnitures" element={<FurnitureHome />} />
+        <Route path="*" element={<Navigate to="/cleanz" replace />} />
+      </Routes>
+      <Footer />
+      <FloatingWhatsApp />
+    </>
+  );
+}
 
 function App() {
   return (
-    <>
-      <UnderConstruction />
-    </>
-  )
+    <BrowserRouter>
+      <ThemeProvider>
+        <AppContent />
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
